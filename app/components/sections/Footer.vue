4cc3aded92cdf6a9677df0fcd83f5d095abfa2a3
Matt <matteofelicidesign@gmail.com>
2025-12-16 21:35:25 +0000

    fix sitemap

--- app/components/sections/Footer.vue
+++ app/components/sections/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="m-auto my-32 max-w-6xl">
     <div class="border-primary flex justify-between border-t pt-6">
-      <h6>© 2025 Therapy4you</h6>
+      <h6>© 2025 Therapy4you. test</h6>
       <h6>Designed & Built by Matteo Felici</h6>
     </div>
   </section>
